37c6d7fb6de1d5049805448f24ae43374f8b135c
Pyry Kuussaari <pyry@kuussaari.fi>
2020-03-13 17:00:13 +0200

    path optimization works

--- src/result.c
+++ src/result.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   result.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pkuussaa <pkuussaa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/12 15:17:55 by pkuussaa          #+#    #+#             */
-/*   Updated: 2020/03/12 17:51:23 by pkuussaa         ###   ########.fr       */
+/*   Updated: 2020/03/13 16:44:40 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,6 +80,37 @@ char	**init_moves(char **arr, int i)
 	return (arr);
 }
 
+int		get_next_move(char **arr, char *name, int i)
+{
+	int		x;
+
+	x = 0;
+		while (arr[i][x])
+		{
+			if (arr[i][x] == '-' && arr[i][x + 1] != '\0')
+			{
+				if (ft_strncmp(arr[i] + x + 1, name, ft_strlen(name)) == 0)
+				{
+					i++;
+					x = 0;
+				}
+			}
+			x++;
+		}
+	return (i);
+}
+
+void	print_moves(char **arr, int i)
+{
+	while (arr[i])
+	{
+		ft_printf("%s\n", arr[i]);
+		if (arr[i][0] == '\0')
+			break ;
+		i++;
+	}
+}
+
 void	optimizate_paths_to_use(t_lemin *lemin, t_room *room)
 {
 	int		count;
@@ -100,15 +131,16 @@ void	optimizate_paths_to_use(t_lemin *lemin, t_room *room)
 	temp->count++;
 	moves[i] = display_format(tmp, ft_itoa(ants));
 	ft_strdel(&tmp);
-	ft_printf("%s\n", moves[0]);
+	//ft_printf("%s %d\n", moves[0], i);
 	while (ants < lemin->ants)
 	{
-		if (temp->count + path_length(lemin->result_paths[current]) >
+		if (find_room(room, get_first_path_node(lemin->result_paths[current]))->count + path_length(lemin->result_paths[current]) >
 		find_room(room, get_first_path_node(lemin->result_paths[get_next_path(lemin->result_paths, current)]))->count + path_length(lemin->result_paths[get_next_path(lemin->result_paths, current)]))
 		{
-			ft_printf("name: %s\n", find_room(room, get_first_path_node(lemin->result_paths[get_next_path(lemin->result_paths, current)]))->name);
+			i = get_next_move(moves, get_first_path_node(lemin->result_paths[get_next_path(lemin->result_paths, current)]), 0);
+			//ft_printf("name: %s\n", find_room(room, get_first_path_node(lemin->result_paths[get_next_path(lemin->result_paths, current)]))->name);
 			tmp = display_format(get_first_path_node(lemin->result_paths[get_next_path(lemin->result_paths, current)]), ft_itoa(ants + 1));
-			ft_printf("tmp: %s\n", tmp);
+			//ft_printf("tmp: %s\n", tmp);
 			ants++;
 			if (moves[i][0] != '\0')
 			{
@@ -118,17 +150,16 @@ void	optimizate_paths_to_use(t_lemin *lemin, t_room *room)
 			else
 				moves[i] = ft_strdup(tmp);
 			ft_strdel(&tmp);
-			ft_printf("%s %d\n", moves[i], i);
-			//temp = find_room(room, get_first_path_node(lemin->result_paths[get_next_path(lemin->result_paths, current)]));
+			//ft_printf("%s %d\n", moves[i], i);
+			find_room(room, get_first_path_node(lemin->result_paths[get_next_path(lemin->result_paths, current)]))->count++;
 			current = get_next_path(lemin->result_paths, current);
-			ft_printf("temp %s\n", temp->name);
 		}
-		else
+		else if (ft_strcmp(lemin->result_paths[0], lemin->result_paths[current]) != 0 &&
+		find_room(room, get_first_path_node(lemin->result_paths[current]))->count + path_length(lemin->result_paths[current]) >
+		find_room(room, get_first_path_node(lemin->result_paths[0]))->count + path_length(lemin->result_paths[0]))
 		{
-			//break ;
-			i++;
-			tmp = display_format(get_first_path_node(lemin->result_paths[current]), ft_itoa(ants + 1));
-			ft_printf("%d\n", i);
+			i = get_next_move(moves, get_first_path_node(lemin->result_paths[0]), 0);
+			tmp = display_format(get_first_path_node(lemin->result_paths[0]), ft_itoa(ants + 1));
 			ants++;
 			if (moves[i][0] != '\0')
 			{
@@ -137,7 +168,26 @@ void	optimizate_paths_to_use(t_lemin *lemin, t_room *room)
 			}
 			else
 				moves[i] = ft_strdup(tmp);
-			ft_printf("%s %d\n", moves[i], i);
+			ft_strdel(&tmp);
+			find_room(room, get_first_path_node(lemin->result_paths[0]))->count++;
+			current = 0;
+		}
+		else
+		{
+			i = get_next_move(moves, get_first_path_node(lemin->result_paths[current]), 0);
+			tmp = display_format(get_first_path_node(lemin->result_paths[current]), ft_itoa(ants + 1));
+			//ft_printf("%d\n", i);
+			ants++;
+			if (moves[i][0] != '\0')
+			{
+				tmp = ft_strjoin(" ", tmp);
+				moves[i] = ft_strjoin(moves[i], tmp);
+			}
+			else
+				moves[i] = ft_strdup(tmp);
+			find_room(room, get_first_path_node(lemin->result_paths[current]))->count++;
+			//ft_printf("%s %d\n", moves[i], i);
 		}
 	}
+	print_moves(moves, 0);
 }
